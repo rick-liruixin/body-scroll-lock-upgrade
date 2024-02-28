a8063db6753038486e703ecc10a617b90283dc7b
李瑞鑫 <15939942017@163.com>
2024-02-28 14:21:18 +0800

    next bug

--- examples/next/next.config.js
+++ examples/next/next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const generateBuildId = () => {
+  if (process.env.APP_VERSION) {
+    return process.env.APP_VERSION
+  }
+  try {
+    const commitHash = require('child_process')
+      .execSync('git rev-parse HEAD')
+      .toString()
+      .toLowerCase()
+      .trim()
+    return commitHash
+  } catch (error) {
+    console.error('[nextConfig][commitHash]', error)
+  }
+  return Date.now() + ''
+}
+
+const nextConfig = {
+  generateBuildId,
+}
 
 module.exports = nextConfig
